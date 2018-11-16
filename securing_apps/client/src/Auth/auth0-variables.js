82236f4f87ad81477feb9b37774994e7d77ca662
Ulf Svensson <ulf.e.sv@gmail.com>
2018-11-16 09:39:26 +0100

    Add files via upload

---
+++ securing_apps/client/src/Auth/auth0-variables.js
@@ -0,0 +1,6 @@
+export const AUTH_CONFIG = {
+  domain: 'mannydesigns.auth0.com',
+  clientId: '9woV8xlfwyh3YfRCOcc9j9p5T3N6Jl4X',
+  callbackUrl: 'http://localhost:3000/callback',
+  apiUrl: 'https://manny-linkedin'
+}
